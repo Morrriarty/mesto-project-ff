dda1ba525936d5e0bd595c2ddae11fbd0d280d25
Morrriarty <159139396+Morrriarty@users.noreply.github.com>
2024-06-11 01:06:50 +0500

    first iter

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Восточно-Казахстанская область",
+    link: "https://images.unsplash.com/photo-1622856459600-d945a495f478?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Западно-Казахстанская область",
+    link: "https://images.unsplash.com/photo-1717614689977-f41112652588?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Центрально-Казахстанская область",
+    link: "https://images.unsplash.com/photo-1717226411474-3602bbd2c610?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Алматы",
+    link: "https://images.unsplash.com/photo-1711667127098-0256d6ecdaaa?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Астана",
+    link: "https://images.unsplash.com/photo-1716220858726-425c57e94b63?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Шымкент",
+    link: "https://images.unsplash.com/photo-1716529083046-0059954c3855?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+//template
+const cardTemplate = document.querySelector('#card-template').content;
+//type of DOM
+const container = document.querySelector('.content'); //класс <main>
+const cardsContainer = container.querySelector('.places__list');
+//add card function
+function createCard(deleteCard) {
+    initialCards.forEach(function (element) {
+        const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+        const deleteButton = cardElement.querySelector('.card__delete-button');
+        cardElement.querySelector('.card__title').textContent = element.name;
+        cardElement.querySelector('.card__image').src = element.link;
+        cardElement.querySelector('.card__image').alt = element.name;
+        cardsContainer.append(cardElement);
+        deleteButton.addEventListener('click', () => {
+            deleteCard(cardElement);
+        });
+    return cardElement;
+})}
+//remove card function
+function deleteCard(cardElement) {
+        cardElement.remove();
+}
+//display on page
+createCard(deleteCard);
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
