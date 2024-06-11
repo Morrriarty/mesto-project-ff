c75949cf7441c9f5c4bed78ebdb62d32c038d6a8
Morrriarty <159139396+Morrriarty@users.noreply.github.com>
2024-06-12 00:08:20 +0500

    second iter

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,38 @@
 //template
 const cardTemplate = document.querySelector('#card-template').content;
+
 //type of DOM
 const container = document.querySelector('.content'); //класс <main>
 const cardsContainer = container.querySelector('.places__list');
+
 //add card function
-function createCard(deleteCard) {
-    initialCards.forEach(function (element) {
-        const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-        const deleteButton = cardElement.querySelector('.card__delete-button');
-        cardElement.querySelector('.card__title').textContent = element.name;
-        cardElement.querySelector('.card__image').src = element.link;
-        cardElement.querySelector('.card__image').alt = element.name;
-        cardsContainer.append(cardElement);
-        deleteButton.addEventListener('click', () => {
-            deleteCard(cardElement);
-        });
+function createCard(element) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImage = cardElement.querySelector('.card__image');
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardImage.src = element.link;
+    cardImage.alt = element.name;
+
+//event handler
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardElement);
+    })
     return cardElement;
-})}
+}
+//array render function
+function renderCard(cardElement) {
+    cardsContainer.append(createCard(cardElement));
+}
+
+initialCards.forEach(cardElement => {
+    renderCard(cardElement);
+});
+
 //remove card function
 function deleteCard(cardElement) {
         cardElement.remove();
 }
-//display on page
-createCard(deleteCard);
+
+
 
